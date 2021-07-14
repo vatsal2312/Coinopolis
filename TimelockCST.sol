ca0e022b5e54884acfe79fee8d299986033879cd
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-07-14 11:59:46 +0530

    Create TimelockCST.sol

---
+++ TimelockCST.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.7.4;
+
+// Must import CST AND add the correct CST address in constructor
+import '../dBankV2/CST.sol';
+
+
+contract Timelock {
+  uint public constant duration = 365 days;
+  uint public immutable end;
+  address public immutable owner;
+  CST public cst;
+
+  constructor() {
+    end = block.timestamp + duration;
+    owner = msg.sender; 
+    cst = CST();
+  }
+
+  function withdraw() external {
+    require(msg.sender == owner, 'only owner');
+    require(block.timestamp >= end, 'too early');
+    cst.transferOwnership(owner);
+  }
+}
